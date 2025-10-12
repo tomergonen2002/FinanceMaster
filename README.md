301be472c4de402da799fd1513c141b8afcc26ef
Tomer Gonen <s0596740@htw-berlin.de>
2025-10-12 15:38:26 +0200

    M1: FinanceMaster launched

---
+++ README.md
@@ -0,0 +1,7 @@
+# WebTech WiSe25/26 – Gruppenarbeit
+Team Mitglieder: Tomer Gonen, Kolja Schollmeyer
+
+Wir entwickeln eine Finanzplattform zur Verwaltung von Einnahmen/Ausnahmen.
+
+# Quellen
+1. JPA Entity Classes auf YT: https://www.youtube.com/watch?v=yiDuRC12vLE&t=8s
---
+++ src/main/java/com/example/rest_service/Controller.java
@@ -0,0 +1,22 @@
+package com.example.rest_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to FinanceMaster!";
+    }
+
+    @GetMapping("/categories")
+    public List<Category> getCategories() {
+        return List.of(
+                new Category("Gehalt", "Monatliches Einkommen"),
+                new Category("Lebensmittel", "Wöchentliche Einkäufe")
+        );
+    }
+}
